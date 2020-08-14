4d326fb44cbdca8bc0ae3f6496321fa929ed363c
Edson Famanda <edson@equipmoz.org>
2020-08-14 12:25:12 +0200

    Update Button Done

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -9,6 +9,19 @@
 
                 <div class="card-body">
 
+                    <form action="{{ route('admin.users.update', ['user' => $user->id]) }}" method="POST">
+                        @csrf
+                        {{ method_field('PUT') }}
+                        @foreach($roles as $role)
+                            <div class="form-check">
+                                <input type="checkbox" name="roles[]" value="{{ $role->id }}"
+                                        {{ $user->hasAnyRole($role->name) ? 'checked' : '' }}>
+                                <label> {{$role->name}} </label> 
+                            </div>
+                        @endforeach    
+                    <button type="submit" class="btn btn-primary"> Update </button>
+                    </form>
+
                 </div>
             </div>
         </div>
